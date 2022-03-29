a18a5aaacf3a9e97abf3f8d79a054340c4dca8a5
JasperStFun <kosto55@mail.ru>
2022-03-29 10:45:47 +0300

    pafination

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 
 from .models import *
@@ -14,10 +15,17 @@ from .serializers import *
 from .permissions import *
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
